2746f87ce86b067d15f34c2bf38304b5c92e588f
1kot1k1 <243334885+1kot1k1@users.noreply.github.com>
2026-05-30 22:03:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using AzureYachts.Data; // Добавь это в самый верх файла!
+using Microsoft.EntityFrameworkCore; // И это тоже!
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+// --- БЛОК ЗАГРУЗКИ ДАННЫХ ---
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex)
+    {
+        // Если что-то пойдет не так при создании базы
+        Console.WriteLine("Ошибка при создании базы: " + ex.Message);
+    }
+}
+// -----------------------------
+
+app.Run();
+app.Run();
